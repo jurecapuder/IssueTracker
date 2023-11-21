571884e1f8dc3ba65096f931f27d019feab7eff1
jurecapuder <capuder.j@gmail.com>
2023-11-21 14:27:50 +0100

    Fix styling issue in NavBar component

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -7,7 +7,7 @@ import { FaBug } from "react-icons/fa";
 
 const NavBar = () => {
   const currentPath = usePathname();
-  
+
   const links = [
     { label: "Dashboard", href: "/" },
     { label: "Issues", href: "/issues" },
@@ -23,7 +23,7 @@ const NavBar = () => {
         {links.map(link => (
           <Link
             key={link.href}
-            className="text-zinc-500 hover:text-zinc-800 transition-colors"
+            className={`${link.href === currentPath ? "text-zinc-900" : "text-zinc-500"} hover:text-zinc-800 transition-colors`}
             href={link.href}
           >
             {link.label}
