6b79cbf028cea3c621de3af8de8a01471f303951
jurecapuder <capuder.j@gmail.com>
2023-11-21 14:16:34 +0100

    Add links to NavBar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -3,6 +3,11 @@ import React from 'react'
 import { FaBug } from "react-icons/fa";
 
 const NavBar = () => {
+  const links = [
+    { label: "Dashboard", href: "/" },
+    { label: "Issues", href: "/issues" },
+  ]
+
   return (
     <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
       <Link href="/">
@@ -10,17 +15,15 @@ const NavBar = () => {
       </Link>
 
       <ul className="flex space-x-6">
-        <li>
-          <Link href="/">
-            Dashboard
+        {links.map(link => (
+          <Link
+            key={link.href}
+            className="text-zinc-500 hover:text-zinc-800 transition-colors"
+            href={link.href}
+          >
+            {link.label}
           </Link>
-        </li>
-
-        <li>
-          <Link href="/issues">
-            Issues
-          </Link>
-        </li>
+        ))}
       </ul>
     </nav>
   )
