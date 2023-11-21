aa81ac9a3829597517268874c612a9a1d2a7d607
jurecapuder <capuder.j@gmail.com>
2023-11-21 14:30:05 +0100

    Refactor NavBar className using classnames library

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -4,6 +4,7 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 import React from 'react'
 import { FaBug } from "react-icons/fa";
+import classnames from "classnames";
 
 const NavBar = () => {
   const currentPath = usePathname();
@@ -23,7 +24,11 @@ const NavBar = () => {
         {links.map(link => (
           <Link
             key={link.href}
-            className={`${link.href === currentPath ? "text-zinc-900" : "text-zinc-500"} hover:text-zinc-800 transition-colors`}
+            className={classnames({
+              "text-zinc-900" : link.href === currentPath,
+              "text-zinc-500" : link.href !== currentPath,
+              "hover:text-zinc-800 transition-colors" : true
+            })}
             href={link.href}
           >
             {link.label}
