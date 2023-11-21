21a8afff8cc6906a199d98020b6b9c05e1c86748
jurecapuder <capuder.j@gmail.com>
2023-11-21 14:13:03 +0100

    Update logo in NavBar component

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,11 +1,12 @@
 import Link from "next/link";
 import React from 'react'
+import { FaBug } from "react-icons/fa";
 
 const NavBar = () => {
   return (
     <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
       <Link href="/">
-        Logo
+        <FaBug />
       </Link>
 
       <ul className="flex space-x-6">
